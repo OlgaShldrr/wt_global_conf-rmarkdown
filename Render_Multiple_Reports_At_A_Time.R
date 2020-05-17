fdfb9fa5d59a4f29e2202eb3fbda98fd2d061a0b
OlgaShldrr <sholderer@gmail.com>
2020-05-17 17:37:41 +0200

    initial commit

---
+++ Render_Multiple_Reports_At_A_Time.R
@@ -0,0 +1,10 @@
+countries <- c("Austria", "Brazil", "Poland")
+countries <- MOCK_DATA %>% select(country) %>% unique() %>% as.matrix()
+
+for (i in 1:length(countries)) {
+rmarkdown::render(output_file = paste0("multiple_reports/Monthly_Report_Template_", countries[i], ".Rmd"),
+                  input = "Monthly_Report_Template_Country.Rmd", 
+                  params = list(
+                    country = countries[i] 
+))
+}
